50fb00e95ca59806e6b8d8f63ca3440443310dde
Marcos Menezes <marcosnunes12340987@gmail.com>
2025-12-03 18:54:27 -0300

    Getting all lines

--- client/src/api/pdfExtractor.js
+++ client/src/api/pdfExtractor.js
@@ -1,3 +1,32 @@
+function groupWordsIntoLines(words, tolerance = 20) {
+  const sortedWords = [...words].sort((a, b) => a.box.top - b.box.top);
+
+  const lines = [];
+  let currentLine = [];
+  
+  let lastTop = sortedWords[0].box.top;
+
+  for (const word of sortedWords) {
+    const currentTop = word.box.top;
+
+    if (Math.abs(currentTop - lastTop) <= tolerance) {
+      currentLine.push(word);
+    } else {
+      currentLine.sort((a, b) => a.box.left - b.box.left);
+      lines.push(currentLine);
+
+      currentLine = [word];
+      lastTop = currentTop;
+    }
+  }
+
+  if (currentLine.length > 0) {
+    currentLine.sort((a, b) => a.box.left - b.box.left);
+    lines.push(currentLine);
+  }
+
+  return lines;
+}
 
 export const extractTextFromPDF = async (file) => {
     const formData = new FormData();
@@ -11,6 +40,15 @@ export const extractTextFromPDF = async (file) => {
 
         const data = await response.json();
         console.log("Resposta do OCR:", data);
+
+        let allLines = []
+        for(const page of data.result) {
+            const lines = groupWordsIntoLines(page.words);
+            console.log("Lines on page ", page.page, ":", lines);
+            allLines = allLines.concat(lines);
+        }
+
+        console.log("ALL LINES:", allLines)
 
         return fullText;
     } catch (error) {
